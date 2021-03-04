573f28e2b2fb6b75e3a919589cf69c9ddb442302
Anthony <anthonybilodeau214@gmail.com>
2021-03-04 14:37:33 -0500

    added ilastik in actin

--- src/CellTrackingChallenge/predict.py
+++ src/CellTrackingChallenge/predict.py
@@ -92,7 +92,7 @@ class Predicter:
         """
         Loads the model from model_path
         """
-        net_params, trainer_params = self.load(self.model_path)
+        net_params, trainer_params = self.load()
         trainer_params["size"] = self.size
         self.model = MICRANet(grad=True, **trainer_params)
         self.model.eval()
@@ -109,12 +109,16 @@ class Predicter:
         io.imsave(os.path.join(self.save_folder, "{}_precise.tif".format(batch)), precise.astype(numpy.float32), check_contrast=False)
         io.imsave(os.path.join(self.save_folder, "{}_rawprecise.tif".format(batch)), raw_precise.astype(numpy.float32), check_contrast=False)
 
-    def load(self, folder):
+    def load(self):
         """
         Loads a previous network and optimizer state
         """
-        net_params = torch.load(os.path.join(folder, "params.net"), map_location=None if self.cuda else "cpu")
-        trainer_params = json.load(open(os.path.join(folder, "trainer_params.json"), "r"))
+        with h5py.File(os.path.join(self.model_path, "CTCModelZoo.hdf5"), "r") as file:
+            networks = {}
+            for key, values in file["MICRANet"].items():
+                networks[key] = {k : torch.tensor(v[()]) for k, v in values.items()}
+            trainer_params = json.loads(file["MICRANet"].attrs["trainer_params"])
+        net_params = networks[key]
         return net_params, trainer_params
 
 if __name__ == "__main__":
